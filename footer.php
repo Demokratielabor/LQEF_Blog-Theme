e22dbb7a4ecfb3deed169a96e2bd0a3a1db99e46
Philipp <accounts@philipp-lehmann.eu>
2013-04-07 21:24:32 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+  </div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'twentytwelve_credits' ); ?>
+<small><span id="copyleft">&copy;</span> Liquid Erfurt 2013.<br />
+                    <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Dieser Blog</span> von <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.liquid-erfurt.org/" property="cc:attributionName" rel="cc:attributionURL">Liquid Erfurt</a> steht unter einer <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/de/">Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 3.0 Deutschland Lizenz</a>.<br />
+<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a> </small>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
