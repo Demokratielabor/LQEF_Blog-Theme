912142d26c190f0c9460c83e7ea04c27b5e121b7
Thomas Sänger <HorayNarea@gmail.com>
2014-06-12 02:45:54 +0200

    WordPress-foo entfernt

--- footer.php
+++ footer.php
@@ -17,7 +17,6 @@
 			<small>
 				&copy; 2013-2014 Liquid Erfurt e.V.<br />
 				<span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Dieses Blog</span> von <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.liquid-erfurt.org/" property="cc:attributionName" rel="cc:attributionURL">Liquid Erfurt</a> steht unter einer <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/de/">Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 3.0 Deutschland Lizenz</a>.<br />
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
 			</small>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
